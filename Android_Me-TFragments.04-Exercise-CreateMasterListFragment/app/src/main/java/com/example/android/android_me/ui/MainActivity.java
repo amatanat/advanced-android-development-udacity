aca919783e9f599ae1b76f34dad5c06f647d82bf
Matanat Ahmadova <ahmadova.matanat01@gmail.com>
2017-08-18 11:04:23 +0200

    pass clicked items position from MainActivity to AndroidMeActivity - communicate between activities

--- Android_Me-TFragments.04-Exercise-CreateMasterListFragment/app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ Android_Me-TFragments.04-Exercise-CreateMasterListFragment/app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.android.android_me.R;
 
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClicked{
+
+    private int headIndex;
+    private int bodyIndex;
+    private int legIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,39 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
 
     @Override
     public void OnImageSelected(int position) {
-        Toast.makeText(this, "Clicked image: " + position, Toast.LENGTH_LONG).show();
+
+        int bodyPartNumber = position / 12;
+         int listIndex = position - 12 * bodyPartNumber;
+
+         switch (listIndex){
+             case 0:
+                 headIndex = listIndex;
+                 break;
+             case 1:
+                 bodyIndex = listIndex;
+                 break;
+             case 3:
+                 legIndex = listIndex;
+                 break;
+             default:
+                 break;
+         }
+
+         Bundle bundle = new Bundle();
+         bundle.putInt("headIndex", headIndex);
+         bundle.putInt("bodyIndex", bodyIndex);
+         bundle.putInt("legIndex", legIndex);
+
+        final Intent intent = new Intent(this, AndroidMeActivity.class);
+         intent.putExtras(bundle);
+
+        Button button = (Button) findViewById(R.id.button_next);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(intent);
+            }
+        });
     }
 }
