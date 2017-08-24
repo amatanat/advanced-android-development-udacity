8ef0e8b87bf7693d281f2f321569b140ba7b6719
Matanat Ahmadova <ahmadova.matanat01@gmail.com>
2017-08-24 21:07:47 +0200

    Create espresso test using espresso test recorder

---
+++ Lesson8 - Espresso/AdvancedAndroid_TeaTime/app/src/androidTest/java/com/example/android/teatime/EspressoTestRecorderTest.java
@@ -0,0 +1,97 @@
+package com.example.android.teatime;
+
+
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class EspressoTestRecorderTest {
+
+    @Rule
+    public ActivityTestRule<MenuActivity> mActivityTestRule = new ActivityTestRule<>(MenuActivity.class);
+
+    @Test
+    public void espressoTestRecorderTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        DataInteraction linearLayout = onData(anything())
+                .inAdapterView(allOf(withId(R.id.tea_grid_view),
+                        childAtPosition(
+                                withId(R.id.activity_main),
+                                1)))
+                .atPosition(2);
+        linearLayout.perform(click());
+        
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.tea_name_text_view), withText("White Tea"),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_tea_detail_header),
+                                        childAtPosition(
+                                                withId(R.id.activity_tea_detail),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        textView2.check(matches(withText("White Tea")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.cost_text_view), withText("$0.00"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.activity_tea_detail),
+                                        10),
+                                1),
+                        isDisplayed()));
+        textView3.check(matches(withText("$0.00")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
