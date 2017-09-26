e2108a441d31773fc2816496a62784b7c03c7f50
Matanat Ahmadova <ahmadova.matanat01@gmail.com>
2017-09-26 11:16:32 +0200

    Add a watering Service to a widget

--- Lesson7-Widgets/AdvancedAndroid_MyGarden-TWID.01-Exercise-AddNewWidget/app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ Lesson7-Widgets/AdvancedAndroid_MyGarden-TWID.01-Exercise-AddNewWidget/app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -1,6 +1,7 @@
 package com.example.android.mygarden;
 
 import android.app.PendingIntent;
+import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
@@ -23,9 +24,24 @@ public class PlantWidgetProvider extends AppWidgetProvider {
         // create intent for MainActivity
         Intent configIntent = new Intent(context, MainActivity.class);
         // create pending intent
-        PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+        PendingIntent configPendingIntent =
+                PendingIntent.getActivity(context, 0, configIntent, 0);
+
         // perform imageview click on RemoteView
-        views.setOnClickPendingIntent(R.id.appwidget_image, configPendingIntent);
+        views.setOnClickPendingIntent(R.id.widget_plant_image, configPendingIntent);
+
+        // perform background operation on 'water plant' image click
+        // create intent for service and set its action
+        // create pending intent
+        // set click event
+
+        Intent wateringIntent = new Intent(context, PlantWateringService.class);
+        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
+
+        PendingIntent pendingIntent =
+                PendingIntent.getService(context, 0,wateringIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setOnClickPendingIntent(R.id.widget_water_button, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
