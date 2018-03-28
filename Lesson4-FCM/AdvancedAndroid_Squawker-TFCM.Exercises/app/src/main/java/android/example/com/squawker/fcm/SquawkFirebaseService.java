f1b4ca6ea6d33647fb2fb2254ff6bcc91e207bd0
Matanat Ahmadova <ahmadova.matanat01@gmail.com>
2018-03-28 21:50:19 +0200

    Update gradle file. Update source code: get FCM token.

---
+++ Lesson4-FCM/AdvancedAndroid_Squawker-TFCM.Exercises/app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseService.java
@@ -0,0 +1,17 @@
+package android.example.com.squawker.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class SquawkFirebaseService extends FirebaseInstanceIdService {
+
+    private final String TAG = SquawkFirebaseService.class.getName();
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    }
+}
